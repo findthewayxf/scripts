8b9ea75b379b0f5914a0150b831a6358f616c5ca
findthewayxf <findthewayxf@gmail.com>
2023-03-01 19:51:56 +0800

    tcping.sh

--- tcping.sh
+++ tcping.sh
@@ -2,19 +2,9 @@
 
 mkdir -p /usr/local/tcping
 wget https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
-if [ -f tcping-linux-amd64-v0.1.1.tar.gz ]; then
-  echo "Download succeeded"
-else
-  echo "Download failed"
-  exit 1
+if [ $? -eq 0 ]; then
+    tar -zxvf tcping-linux-amd64-v0.1.1.tar.gz -C /usr/local/tcping
+    echo 'export PATH=$PATH:/usr/local/tcping' >> ~/.bashrc
+    source ~/.bashrc
 fi
-tar -zxvf tcping-linux-amd64-v0.1.1.tar.gz -C /usr/local/tcping
-if [ -f /usr/local/tcping/tcping ]; then
-  echo "Extract succeeded"
-else
-  echo "Extract failed"
-  exit 1
-fi
-echo 'export PATH=$PATH:/usr/local/tcping' >> ~/.bashrc
-source ~/.bashrc
 rm -f tcping*
