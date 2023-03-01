3ef11c0668b00b484c50447b8a09fe2ada3d2b7b
findthewayxf <findthewayxf@gmail.com>
2023-03-01 20:18:37 +0800

    优化

--- tcping.sh
+++ tcping.sh
@@ -8,13 +8,13 @@ fi
 # 检查是否已经下载过文件
 if [ ! -f "/usr/local/tcping/tcping" ]; then
     # 下载文件到/tmp
-    wget -P /tmp https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
+    wget -P /tmp https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar
 
-    # 将文件复制到/usr/local/tcping
-    cp /tmp/tcping-linux-amd64-v0.1.1/tcping /usr/local/tcping/
+    # 解压文件到/usr/local/tcping
+    tar -xf /tmp/tcping-linux-amd64-v0.1.1.tar -C /usr/local/tcping --strip-components=1 tcping
 
     # 删除下载的文件
-    rm -rf /tmp/tcping-linux-amd64-v0.1.1
+    rm /tmp/tcping-linux-amd64-v0.1.1.tar
 fi
 
 # 检查~/.bashrc是否已经包含export PATH=$PATH:/usr/local/tcping，如果没有则追加
