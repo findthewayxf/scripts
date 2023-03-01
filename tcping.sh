f8d49145a4b23d36e305a2e1b89621eeea409e0f
xf <45526014+findthewayxf@users.noreply.github.com>
2023-03-01 01:22:25 -0800

    Update tcping.sh

--- tcping.sh
+++ tcping.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 mkdir -p /usr/local/tcping
-wget https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
-tar -zxvf tcping-linux-amd64-v0.1.1.tar.gz -C /usr/local/tcping
-echo 'export PATH=$PATH:/usr/local/tcping' >> ~/.bashrc
+
+wget -q https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz -O /tmp/tcping.tar.gz
+tar -zxf /tmp/tcping.tar.gz -C /usr/local/tcping --strip-components 1
+rm -f /tmp/tcping.tar.gz
+
+echo 'export PATH="$PATH:/usr/local/tcping"' >> ~/.bashrc
 source ~/.bashrc
-rm -f tcping*
