3f630b1b4e3f48d83dfca2b8659b3e00a14619ef
findthewayxf <findthewayxf@gmail.com>
2023-03-01 19:56:16 +0800

    修改错误

--- tcping.sh
+++ tcping.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-mkdir -p /usr/local/tcping
-wget https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
-if [ $? -eq 0 ]; then
-    tar -zxvf tcping-linux-amd64-v0.1.1.tar.gz -C /usr/local/tcping
-    echo 'export PATH=$PATH:/usr/local/tcping' >> ~/.bashrc
-    source ~/.bashrc
+# 检查是否已经下载过文件
+if [ ! -f "tcping-linux-amd64-v0.1.1.tar.gz" ]; then
+    wget https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
 fi
-rm -f tcping*
+
+# 解压文件到/usr/local/tcping
+tar -zxvf tcping-linux-amd64-v0.1.1.tar.gz -C /usr/local/tcping
+
+# 检查~/.bashrc是否已经包含export PATH=$PATH:/usr/local/tcping，如果没有则追加
+if ! grep -qxF 'export PATH=$PATH:/usr/local/tcping' ~/.bashrc ; then
+    echo 'export PATH=$PATH:/usr/local/tcping' >> ~/.bashrc
+fi
+
+# 立即生效
+source ~/.bashrc
+
+# 删除下载的文件
+rm -f tcping-linux-amd64-v0.1.1.tar.gz
