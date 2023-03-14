06fec8a98cbd28b7dfff7b59c16be8650d76481d
findthewayxf <findthewayxf@gmail.com>
2023-03-14 21:36:16 +0800

    优化 docker

--- install-docker.sh
+++ install-docker.sh
@@ -29,7 +29,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 # 添加 Docker 的 APT 源
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(cat /etc/debian_version | cut -d '.' -f 1) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # 更新 APT 缓存并安装 Docker
 apt-get update
