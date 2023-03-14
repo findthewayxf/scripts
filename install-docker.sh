54ab46e571d53fa98df248a13736a50420ce4e20
findthewayxf <findthewayxf@gmail.com>
2023-03-14 21:25:11 +0800

    优化 docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+
+# 判断当前用户是否为 root 用户
+if [ "$(id -u)" -ne 0 ]; then
+  echo "请使用 root 用户执行此脚本" >&2
+  exit 1
+fi
+
+# 判断当前系统是否为 Debian 系统
+if ! command -v lsb_release > /dev/null 2>&1 || [[ $(lsb_release -si) != "Debian" ]]; then
+  echo "此脚本只适用于 Debian 系统" >&2
+  exit 1
+fi
+
 # 安装必要的依赖
+apt-get update
 apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
+
+# 清除原有 Docker 源
+rm -f /etc/apt/sources.list.d/docker.list
 
 # 添加 Docker 的 GPG 密钥
 mkdir -p /etc/apt/keyrings
@@ -18,3 +35,10 @@ echo \
 # 更新 APT 缓存并安装 Docker
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# 检查 Docker 是否正常运行
+if ! systemctl status docker > /dev/null 2>&1; then
+  echo "Docker 安装成功，但未能正常运行，请检查 Docker 是否配置正确" >&2
+fi
+
+echo "Docker 安装成功"
