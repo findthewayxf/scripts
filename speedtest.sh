ef7795ab848940dffb6ed5fc2139fe5f2b9b9689
findthewayxf <findthewayxf@gmail.com>
2024-01-20 10:47:31 +0800

    24-01-20-501

--- speedtest.sh
+++ speedtest.sh
@@ -2,14 +2,14 @@
 # https://www.speedtest.net/apps/cli
 
 # 安装 curl
-apt-get update
-apt-get install sudo curl -y
+apt update
+apt install sudo curl -y
 
 # 检查是否已安装 speedtest
 if ! command -v speedtest &> /dev/null; then
     echo "正在安装 speedtest"
 	curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-    apt-get install speedtest -y
+    apt install speedtest -y
 fi
 
 
@@ -20,21 +20,22 @@ if ! command -v speedtest &> /dev/null; then
 fi
 
 # 声明一个空数组来存储结果
-results=()
+#results=()
 
 # 运行第一条命令并将结果添加到数组
-output=$(speedtest -s 54312 --accept-license)
-results+=("$output")
+#output=$(speedtest -s 54312 --accept-license)
+#results+=("$output")
 
 # 运行第二条命令并将结果添加到数组
-output=$(speedtest -s 59386 --accept-license)
-results+=("$output")
+#output=$(speedtest -s 59386 --accept-license)
+#results+=("$output")
 
 # 显示所有结果
-for result in "${results[@]}"; do
-  echo "$result"
-done
+#for result in "${results[@]}"; do
+#  echo "$result"
+#done
+
+rm -f speedtest.sh
 
 exit 0
 
-rm -f speedtest.sh
